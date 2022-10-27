2b4c615aa11c69496601ae72e22c914c68970d62
DongYeon Kang <myjjue00@gmail.com>
2022-10-27 11:49:08 +0900

    Add requestParam - use Map - print console

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.springboot.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class HelloController {
@@ -22,5 +24,12 @@ public class HelloController {
     @GetMapping(value = "/request1")
     public String getVariable2(@RequestParam String name, @RequestParam String email, @RequestParam String organization) {
         return String.format("%s %s %s", name, email, organization);
+    }
+    @GetMapping(value = "/request2")
+    public String getVariable3(@RequestParam Map<String,StringBuilder> params) {
+        params.entrySet().forEach((map)->{
+            System.out.printf("key:%s value:%s\n",map.getKey(),map.getValue());
+        });
+        return "호출 완료";
     }
 }
