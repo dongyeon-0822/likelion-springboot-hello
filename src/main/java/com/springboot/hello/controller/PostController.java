d6d419dffb62a823db4a5a201e03fbab6db8195a
DongYeon Kang <myjjue00@gmail.com>
2022-10-27 14:17:04 +0900

    Add postAPI - use RequestBody - use DTO object

--- src/main/java/com/springboot/hello/controller/PostController.java
+++ src/main/java/com/springboot/hello/controller/PostController.java
@@ -1,8 +1,9 @@
 package com.springboot.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -11,6 +12,18 @@ public class PostController {
     @RequestMapping(value = "/domain" ,method = RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
+    }
+
+    @PostMapping(value = "/member1")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder(); // Builder Pattern
+        postData.entrySet().forEach(map->sb.append(map.getKey()+":"+map.getValue()+"\n"));
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMember2(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
     }
 }
 
