00b5b7cfb4064bb036bf305e1b01f8d1bc22f8b3
DongYeon Kang <myjjue00@gmail.com>
2022-10-27 11:39:54 +0900

    Create HelloController - Add API(pathVariable, RequestParam)

---
+++ src/test/java/com/springboot/hello/HelloApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springboot.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
