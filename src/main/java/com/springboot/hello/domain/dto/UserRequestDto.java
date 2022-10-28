f66a56b0c5f34a1ad7ded75e2e933c954f5d00ea
DongYeon Kang <myjjue00@gmail.com>
2022-10-28 14:11:35 +0900

    Create UserRequestDto.java

---
+++ src/main/java/com/springboot/hello/domain/dto/UserRequestDto.java
@@ -0,0 +1,42 @@
+package com.springboot.hello.domain.dto;
+
+public class UserRequestDto {
+    private String id;
+    private String name;
+    private String password;
+
+    public UserRequestDto(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s", this.id, this.name, this.password);
+    }
+}
