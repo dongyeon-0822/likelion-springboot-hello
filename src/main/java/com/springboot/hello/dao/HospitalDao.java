cf4da1fb05129b9645385df3811755d761bb46d1
DongYeon Kang <myjjue00@gmail.com>
2022-11-02 11:17:02 +0900

    Modify HospitalDao, HospitalParserTest.java

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -2,6 +2,7 @@ package com.springboot.hello.dao;
 
 import com.springboot.hello.domain.Hospital;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +12,31 @@ public class HospitalDao {
 
     public HospitalDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    RowMapper<Hospital> rowMapper = (rs, rowNum) ->{
+        Hospital hospital = new Hospital();
+        hospital.setId(rs.getInt("id"));
+        hospital.setOpenServiceName(rs.getString("open_service_name"));
+        hospital.setOpenLocalGovernmentCode(rs.getInt("open_local_government_code"));
+        hospital.setManagementNumber(rs.getString("management_number"));
+        hospital.setLicenseDate(rs.getTimestamp("license_date").toLocalDateTime());
+        hospital.setBusinessStatus(rs.getInt("business_status"));
+        hospital.setBusinessStatusCode(rs.getInt("business_status_code"));
+        hospital.setPhone(rs.getString("phone"));
+        hospital.setFullAddress(rs.getString("full_address"));
+        hospital.setRoadNameAddress(rs.getString("road_name_address"));
+        hospital.setHospitalName(rs.getString("hospital_name"));
+        hospital.setBusinessTypeName(rs.getString("business_type_name"));
+        hospital.setHealthcareProviderCount(rs.getInt("healthcare_provider_count"));
+        hospital.setPatientRoomCount(rs.getInt("patient_room_count"));
+        hospital.setTotalNumberOfBeds(rs.getInt("total_number_of_beds"));
+        hospital.setTotalAreaSize(rs.getFloat("total_area_size"));
+        return hospital;
+    };
+    public Hospital findById(int id) {
+        return this.jdbcTemplate.queryForObject(
+                "select * from nation_wide_hospital where id = ?;", rowMapper,id);
     }
 
     // List<Hospital>
@@ -38,8 +64,5 @@ public class HospitalDao {
     public void deleteAll() {
         this.jdbcTemplate.update("delete from nation_wide_hospital;");
     }
-
-
-
 }
 
